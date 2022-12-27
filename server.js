7bd4d6842b0144e6c01c5361ed84a56ad6933288
joao garcia <jiboiadamor@gmail.com>
2022-12-27 08:58:24 +0000

    News API being read to my server side project

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+const options = {
+  headers: {
+    'User-Agent': 'JGNews/1.0'
+  }
+};
+
 app.get('/', (req, res) => {
-  request.get(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY}`, (error, response, body) => {
+  request.get(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.API_KEY}`, options, (error, response, body) => {
     if (error) {
       res.send(error);
     } else {
